391d62014f69127d07369206414116ba0a86363c
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-01-14 16:08:28 -0300

    raw version

---
+++ back-end/aircraft-api/src/main/java/com/br/aircraft/AircraftApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.aircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircraftApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircraftApiApplication.class, args);
+	}
+
+}
---
+++ back-end/aircraft-api/src/test/java/com/br/aircraft/AircraftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.aircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AircraftApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
