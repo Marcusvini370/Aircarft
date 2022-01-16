ee417879d92f75557f6c70c6f4cd4e0c58449a7d
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-01-16 11:20:34 -0300

    adding service and model

---
+++ front-end/air-angular/src/app/model/aeronave.ts
@@ -0,0 +1,11 @@
+export class Aeronave {
+
+
+   id!: Number;
+	 nome!: String;
+	 marca!: String;
+	 ano!: Number;
+	 descricao!: String;
+	 vendido!: Boolean;
+
+}
---
+++ front-end/air-angular/src/app/service/aeronave.service.ts
@@ -0,0 +1,18 @@
+import { AppConstants } from './../app-constants';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AeronaveService {
+
+  constructor(private http: HttpClient) { }
+
+  getAeronaveList(): Observable<any> {
+      return this.http.get<any>(AppConstants.baseUrl);
+  }
+
+
+}
