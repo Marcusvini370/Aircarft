b8e2e85a76157604d04d7213c9f9ddb6a2837547
Marcus Vinicius Martins de Matos <51136557+Marcusvini370@users.noreply.github.com>
2022-01-20 16:23:13 -0300

    test deploy

--- front-end/air-angular/src/app/app-constants.ts
+++ front-end/air-angular/src/app/app-constants.ts
@@ -1,6 +1,6 @@
 export class AppConstants {
 
-  public static get baseServidor(): string { return "http://marcus-aircraft.herokuapp.com/" }
+  public static get baseServidor(): string { return "https://marcus-aircraft.herokuapp.com/" }
   public static get baseUrl(): string {return this.baseServidor + "aircraft/aeronaves/"}
 
 }
